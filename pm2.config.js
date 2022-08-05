0f0e9275f0b381dd92360563beba403ebb9c2285
zhaoxin <zhaoxin@lepudigital.com>
2022-08-05 14:15:52 +0800

    feat:修改配置文件

--- pm2.config.js
+++ pm2.config.js
@@ -16,7 +16,7 @@ module.exports = {
             "repo":"git@github.com:choukin/nodegamedemo.git",
             "path":"/www/",
             "ssh_options":"StrictHostKeyChecking=no",
-            "post-setup":"ls -la",
+            "post-setup":"ls -la && npm install",
             "pre-deploy-local":"echo '本地要执行的命令'",
             "post-deploy":"npm install && npm run build && pm2 start pm2.config.js --watch",
             "env":{
