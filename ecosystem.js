2c6d78ba5a65a2a3392fc51fb37cdbb45f70e15c
zhaoxin <zhaoxin@lepudigital.com>
2022-08-05 13:31:12 +0800

    feat:修改配置文件

---
+++ ecosystem.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "apps":[{
+        "name":"nodeGame",
+        "script":"src/servers/server.js",
+        "env":{
+        },
+        "env_production":{
+            "NODE_ENV":"production"
+        }
+    }],
+    "deploy":{
+        "production":{
+            "user":"root",
+            "host":["hw"],
+            "ref":"github/main",
+            "repo":"git@github.com:choukin/nodegamedemo.git",
+            "path":"/www/",
+            "ssh_options":"StrictHostKeyChecking=no",
+            "post-setup":"ls -la",
+            "pre-deploy-local":"echo '本地要执行的命令'",
+            "post-deploy":"npm install && npm run build && pm2 startOrRestart ecosystem.js --env production",
+            "env":{
+                "NODE_ENV":"production"
+            }
+        }
+    }
+}
